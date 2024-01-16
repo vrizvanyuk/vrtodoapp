bab05d54bc85b6719ef10523da109521e60aa9dc
vrizvanyuk <155646798+vrizvanyuk@users.noreply.github.com>
2024-01-16 14:50:00 +0200

    Update appunittest.py

--- appunittest.py
+++ appunittest.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from vrtodoapp import ToDoRecycleView
 
 class TestTodoApp(unittest.TestCase):
 
